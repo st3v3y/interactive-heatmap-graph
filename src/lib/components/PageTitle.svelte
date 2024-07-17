1474d28a99ae34cb2c709a74eec137407b3d9ac8
Stefan Lessle <stefan.lessle@mediakular.com>
2024-07-16 20:33:18 -0500

    Created components for page title and widget: Improved toggle optics

---
+++ src/lib/components/PageTitle.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let title;
+</script>
+
+<div class="flex justify-between flex-wrap sm:flex-nowrap items-center py-2.5 w-full">
+    <h1 class="text-2xl font-inter font-semibold">{title}</h1>
+    <div class="flex gap-x-4 flex-wrap sm:flex-nowrap">
+        <slot />
+    </div>
+</div>
---
+++ src/lib/components/Widget.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let title;
+</script>
+
+<div class="w-full h-auto rounded-xl border border-wtgrey-95 px-5 py-3 bg-white">
+    <div class="flex justify-between flex-wrap sm:flex-nowrap items-center pt-5 gap-x-4">
+        <h2 class="text-sm font-semibold font-inter">{title}</h2>
+        <slot name="controls"></slot>
+    </div>
+    <slot />
+</div>
