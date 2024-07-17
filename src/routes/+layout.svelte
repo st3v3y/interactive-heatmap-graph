8a5fbd383ded4fdf87fd898a076154cc81c41fc2
Stefan Lessle <stefan.lessle@mediakular.com>
2024-07-17 11:18:39 -0500

    Improved page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,10 @@
 	import '../app.css';
 </script>
 
-<Header />
-
-<main class="flex flex-col p-4 sm:p-10 md:p-14">
-	<slot />
-</main>
-
-<Footer />
+<div class="flex flex-col justify-between min-h-[100vh]">
+	<Header />
+	<main class="flex flex-col p-4 sm:p-10 md:p-14 h-full flex-grow max-w-full overflow-x-auto overflow-y-visible">
+		<slot />
+	</main>
+	<Footer />
+</div>
